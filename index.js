acb20f1e798922b5e48ad6c1909fb5d9065d4a14
Gabriel Wu <yingpingwdz@163.com>
2018-03-31 14:51:39 +0800

    add Card

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
+import AlbumDetail from './src/components/AlbumDetail';
 //create a component
 const App = ()=>(
   <View>
